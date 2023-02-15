f0e424e3e776898bd2c414be36574b44dea782dc
Banjo <banjtheman@gmail.com>
2023-02-14 22:56:01 -0500

    init commit

---
+++ aws_quiz_st.py
@@ -0,0 +1,175 @@
+"""
+Purpose:
+    Start AWS Quiz
+"""
+
+# Python imports
+import json
+from typing import Type, Union, Dict, Any, List
+import random
+
+# 3rd party imports
+import streamlit as st
+import utils
+
+
+# List of Questions
+@st.cache_data
+def load_questions():
+    question_list = utils.load_json("trim_aws_ai_gen_questions.json")["question_list"]
+
+    # Randomize list
+    random.shuffle(question_list)
+
+    return question_list
+
+
+# Map questions to chocies
+index_map = {0: "A: ", 1: "B: ", 2: "C: ", 3: "D: "}
+
+
+def sidebar() -> None:
+    """
+    Purpose:
+        Shows the side bar
+    Args:
+        N/A
+    Returns:
+        N/A
+    """
+
+    st.sidebar.image(
+        "https://d1.awsstatic.com/training-and-certification/certification-badges/AWS-Certified-Solutions-Architect-Associate_badge.3419559c682629072f1eb968d59dea0741772c0f.png",
+        use_column_width=True,
+    )
+
+    st.sidebar.markdown(
+        "The AWS Certified Solutions Architect - Associate AI Generated Quiz is a tool designed to help individuals prepare for the exam. It uses GPT-3 to generate scenario-based multiple choice questions and provides a comprehensive explanation for the correct answer as well as resources."
+    )
+
+
+def gen_quiz(question_obj, question, show_scenario, key="my-form"):
+    form = st.form(key=key)
+
+    ans_list = question_obj["answer_choices"]
+    choice_list = []
+    correct_list = []
+    exp_list = []
+
+    scenario = question["scenario"]
+
+    if show_scenario:
+        q_string = f"Scenario: {scenario}\n\nQuestion: {question_obj['question']}"
+    else:
+        q_string = question_obj["question"]
+
+    for choice in ans_list:
+        choice_list.append(choice["answer"])
+        correct_list.append(choice["is_correct"])
+        exp_list.append(choice["explanation"])
+
+    selected_answer = form.radio(q_string, choice_list)
+    submit = form.form_submit_button("Submit")
+
+    curr_index = choice_list.index(selected_answer)
+
+    if submit:
+        # Check if correct
+        curr_index = choice_list.index(selected_answer)
+
+        if str(correct_list[curr_index]).lower() == "true":
+            st.success("Correct! " + index_map[curr_index] + choice_list[curr_index])
+            st.write(exp_list[curr_index])
+
+            with st.expander("Explanations"):
+                for index, item in enumerate(exp_list):
+                    if index == curr_index:
+                        continue
+                    st.error(index_map[index] + choice_list[index])
+                    st.write(item)
+        else:
+            st.error("Wrong! " + index_map[curr_index] + choice_list[curr_index])
+            st.write(exp_list[curr_index])
+
+            with st.expander("Explanations"):
+                for index, item in enumerate(exp_list):
+                    if index == curr_index:
+                        continue
+
+                    elif str(correct_list[index]).lower() == "true":
+                        st.success(index_map[index] + choice_list[index])
+                        st.write(item)
+
+                    else:
+                        st.error(index_map[index] + choice_list[index])
+                        st.write(item)
+
+        # Show resoucres
+        with st.expander("Resources"):
+            domain = question["domain"]
+            task = question["task"]
+            focus = question["focus"]
+            item = question["item"]
+
+            st.subheader(domain)
+            st.write(task)
+            st.write(focus + ": " + item)
+
+            for doc in question["docs"]:
+                st.write(doc)
+
+        with st.expander("Prompt"):
+            prompt = question["prompt"]
+            st.write(prompt)
+
+
+def app() -> None:
+    """
+    Purpose:
+        Controls the app flow
+    Args:
+        N/A
+    Returns:
+        N/A
+    """
+
+    # Spin up the sidebar
+    sidebar()
+    # Load questions
+    question_list = load_questions()
+
+    curr_question = st.number_input(
+        "Question Number:", min_value=0, max_value=len(question_list) - 1
+    )
+
+    # Get question details
+    question = question_list[curr_question]
+    question_json = json.loads(question["question"])
+
+    # Checkbox for scenario
+    show_scenario = st.checkbox(
+        "Show Scenario?", help="Shows the scenario used to generate the question"
+    )
+
+    # Generate the quiz
+    gen_quiz(question_json, question, show_scenario)
+
+
+def main() -> None:
+    """
+    Purpose:
+        Controls the flow of the streamlit app
+    Args:
+        N/A
+    Returns:
+        N/A
+    """
+
+    # Start the streamlit app
+    st.title("AWS Certified Solutions Architect - Associate AI Generated Quiz")
+    st.subheader("Study Smarter, Not Harder")
+    app()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,142 @@
+import json
+import logging
+import os
+from typing import Type, Union, Dict, Any, List
+
+import pandas as pd
+
+
+def write_to_file(file_path: str, file_text: str) -> bool:
+    """
+    Purpose:
+        Write text from a file
+    Args/Requests:
+         file_path: file path
+         file_text: Text of file
+    Return:
+        Status: True if appened, False if failed
+    """
+
+    try:
+        with open(file_path, "w") as myfile:
+            myfile.write(file_text)
+            return True
+
+    except Exception as error:
+        logging.error(error)
+        return False
+
+
+def append_to_file(file_path: str, file_text: str) -> bool:
+    """
+    Purpose:
+        Append text to a file
+    Args/Requests:
+         file_path: file path
+         file_text: Text of file
+    Return:
+        Status: True if appended, False if failed
+    """
+
+    try:
+        with open(file_path, "a") as myfile:
+            myfile.write(file_text)
+            return True
+
+    except Exception as error:
+        logging.error(error)
+        return False
+
+
+def read_from_file(file_path: str) -> str:
+    """
+    Purpose:
+        Read data from a file
+    Args/Requests:
+         file_path: file path
+    Return:
+        read_data: Text from file
+    """
+    try:
+        with open(file_path) as f:
+            read_data = f.read()
+
+    except Exception as error:
+        logging.error(error)
+        return None
+
+    return read_data
+
+
+def make_df_from_dict(dict_obj: Dict[str, List[Any]]) -> pd.DataFrame:
+    """
+    Purpose:
+        turns a dictonary to a pandas dataframe
+    Args/Requests:
+         dict_obj = dictonary object
+    Return:
+        df: Dataframe of the dictonary
+    """
+
+    df_map = {}
+    keys = dict_obj.keys()
+
+    for key in keys:
+        df_map[key] = []
+
+        for val in dict_obj[key]:
+            df_map[key].append(val)
+
+    df = pd.DataFrame.from_dict(df_map)
+    return df
+
+
+def get_cmd_output(cmd: str) -> str:
+    """
+    Purpose:
+        Run a shell command and get the output
+    Args:
+        cmd: The command to run
+    Returns:
+        result_stirng: the result of the command
+    """
+
+    result_stirng = os.popen(cmd).read()
+    return result_stirng
+
+
+def load_json(path_to_json: str) -> Dict[str, Any]:
+    """
+    Purpose:
+        Load json files
+    Args:
+        path_to_json (String): Path to  json file
+    Returns:
+        Conf: JSON file if loaded, else None
+    """
+    try:
+        with open(path_to_json, "r") as config_file:
+            conf = json.load(config_file)
+            return conf
+
+    except Exception as error:
+        logging.error(error)
+        raise TypeError("Invalid JSON file")
+
+
+def save_json(json_path: str, json_data: Any) -> None:
+    """
+    Purpose:
+        Save json files
+    Args:
+        path_to_json (String): Path to  json file
+        json_data: Data to save
+    Returns:
+        N/A
+    """
+    # save sut config
+    try:
+        with open(json_path, "w") as outfile:
+            json.dump(json_data, outfile)
+    except Exception as error:
+        raise OSError(error)
